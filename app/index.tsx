55aa707ecaac364dae3979a33420cf945a805bdd
Agustin Peres <agu.peres3@gmail.com>
2024-11-26 02:31:43 -0300

    Add stack navigation with expo router

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,17 @@
 import { StyleSheet, View } from "react-native";
 import { ShoppingListItems } from "../components/ShoppingListItems";
 import { theme } from "../theme";
+import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Link
+        href="/counter"
+        style={{ textAlign: "center", marginBottom: 18, fontSize: 24 }}
+      >
+        Go to counter
+      </Link>
       <ShoppingListItems name="Coffee" />
       <ShoppingListItems name="Tea" isCompleted={true} />
       <ShoppingListItems name="Sugar" isCompleted />
