7dbe4e47a35c725f90b18e3c5b8ddf712861cc98
Agustin Peres <agu.peres3@gmail.com>
2024-11-26 17:51:46 -0300

    Set up bottom tabs navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,51 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import Feather from "@expo/vector-icons/Feather";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { theme } from "../theme";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Shopping List" }} />
-    </Stack>
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorCerulean }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Shopping List",
+          tabBarIcon: ({ color, size }) => {
+            return <Feather name="list" color={color} size={size}></Feather>;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          tabBarIcon: ({ color, size }) => {
+            return (
+              <AntDesign
+                name="clockcircleo"
+                color={color}
+                size={size}
+              ></AntDesign>
+            );
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="idea"
+        options={{
+          title: "My Idea",
+          tabBarIcon: ({ color, size }) => {
+            return (
+              <FontAwesome5
+                name="lightbulb"
+                color={color}
+                size={size}
+              ></FontAwesome5>
+            );
+          },
+        }}
+      />
+    </Tabs>
   );
 }
