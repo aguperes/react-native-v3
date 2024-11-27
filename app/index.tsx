523bec8eb77bfbf5897bd59934e4fde79e973cac
Agustin Peres <agu.peres3@gmail.com>
2024-11-27 00:58:43 -0300

    Add a text input

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,52 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import { ShoppingListItems } from "../components/ShoppingListItems";
 import { theme } from "../theme";
+import { useState } from "react";
+
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+  isCompleted?: boolean;
+};
+
+const initialList: ShoppingListItemType[] = [
+  { id: "1", name: "Coffee" },
+  { id: "2", name: "Tea" },
+  { id: "3", name: "Milk" },
+];
 
 export default function App() {
+  const [shoppingList, setShoppingList] = useState(initialList);
+  const [item, setItem] = useState("");
+
+  const handleSubmit = () => {
+    if (item) {
+      const newShoppingList = [
+        { id: new Date().toISOString(), name: item },
+        ...shoppingList,
+      ];
+      setShoppingList(newShoppingList);
+      setItem("");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <ShoppingListItems name="Coffee" />
-      <ShoppingListItems name="Tea" isCompleted={true} />
-      <ShoppingListItems name="Sugar" isCompleted />
+      <TextInput
+        onChangeText={(input) => {
+          setItem(input);
+          console.log("Input saved", input);
+        }}
+        value={item}
+        placeholder="E.g. Coffee"
+        style={styles.textInput}
+        returnKeyType="done"
+        onSubmitEditing={handleSubmit}
+      />
+      {shoppingList &&
+        shoppingList.map((item) => (
+          <ShoppingListItems key={item.id} name={item.name} />
+        ))}
     </View>
   );
 }
@@ -17,5 +56,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: theme.colorWhite,
     justifyContent: "center",
+    paddingTop: 12,
+  },
+  textInput: {
+    borderColor: theme.colorGrey,
+    borderWidth: 2,
+    padding: 12,
+    marginHorizontal: 12,
+    marginBottom: 12,
+    fontSize: 18,
+    borderRadius: 50,
   },
 });
